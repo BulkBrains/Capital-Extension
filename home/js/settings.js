86e875b334c939e3d3a403f3beb3acc12dff68b0
Crann Moroney <crann@icrann.com>
2023-09-23 22:38:28 +0100

    Updated settings storage

--- home/js/settings.js
+++ home/js/settings.js
@@ -6,6 +6,5 @@ const engineForm = document.getElementById("engineForm")
 engineForm.onsubmit = (e) => {
     e.preventDefault()
     const engineVal = document.getElementById("engine").value
-
-    
+    localStorage.setItem("engine", engineVal)
 }
