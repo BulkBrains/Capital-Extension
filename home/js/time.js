a1bd27f1b4eab843c13a944fa986aea3d655fb6c
Crann Moroney <crann@icrann.com>
2023-09-23 18:28:51 +0100

    Set time and date straight away

--- home/js/time.js
+++ home/js/time.js
@@ -16,6 +16,23 @@ function ordinalSuffix(i) {
     return i + "th";
 }
 
+const date = new Date()
+let hour = date.getHours()
+let min = date.getMinutes()
+let sec = date.getSeconds()
+
+hour = (hour < 10) ? "0" + hour : hour
+min = (min < 10) ? "0" + min : min
+sec = (sec < 10) ? "0" + sec : sec
+
+const time = hour + ":" + min + ":" + sec
+timeElement.innerText = time
+
+var dateString = ordinalSuffix(date.getDate())
+dateString += ' of ' + date.toLocaleString('en-US', { month: 'long' })
+dateString += ' ' + date.getFullYear()
+dateElement.innerText = dateString
+
 const getTime = () => {
     const date = new Date()
     let hour = date.getHours()
