dc83db5cad3e0bb1e08f0bf557c8014ac2abc443
icrann <crann@icrann.ie>
2024-06-11 22:24:53 +0100

    cleaning up

---
+++ home/js/pexels.js
@@ -0,0 +1,24 @@
+const body = document.getElementsByTagName("body")[0]
+const header = document.getElementById("header")
+
+function invertHex(hex) {
+
+    return (Number(`0x1${hex}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase()
+}
+
+fetch("https://capital.bulkbrains.com/pexels.json")
+.then(response => response.json())
+.then(result => {
+    const picNum = Math.floor(Math.random() * 15)
+    header.style.backgroundImage = "url(" + result.photos[picNum].src.original + ")"
+
+    const picURL = result.photos[picNum].url
+    const picPhotographer = result.photos[picNum].photographer
+    const picPhotographerURL = result.photos[picNum].photographer_url
+    const picAvgColour = result.photos[picNum].avg_color
+    const picInvertColour = invertHex(picAvgColour.replace("#", ""))
+
+    console.log(picInvertColour)
+
+    body.style.color = picInvertColour
+})
