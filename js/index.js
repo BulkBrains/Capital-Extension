9c94b21b403494c35f501a09233d3e6aaa9fe5e2
icrann <crann@icrann.ie>
2024-06-19 17:47:17 +0100

    chore: Update homepage button and add settings button

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var setHomepageButton = document.getElementById("setHomepageButton")
+    const setHomepageButton = document.getElementById("setHomepageButton")
+    const goSettings = document.getElementById("goSettings")
+
     setHomepageButton.addEventListener('click', function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             chrome.storage.sync.set({ homepage: "../home/index.html" }, function() {
             chrome.tabs.update(tabs[0].id, { url: "../home/index.html" })
             })
         })
+    })
+
+    goSettings.addEventListener('click', function() {
+        chrome.tabs.create({ url: "../home/settings.html" })
     })
 })
